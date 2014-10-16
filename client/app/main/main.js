872ea0a6838fe0f2ea9b5ab6c5122dd5de9776e0
Kinnan Kwok <kkwoker@hotmail.com>
2014-10-15 17:50:28 -0700

    Initial scaffolding

---
+++ client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('lolBetApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
