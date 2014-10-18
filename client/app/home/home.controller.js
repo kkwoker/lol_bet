c3a260c99962ab8f3c2deb11a846ab321ed928c1
Marlon Kenny <marlonkenny@gmail.com>
2014-10-18 02:23:57 +0000

    home page mostly working. icon doesn't show sometimes

--- client/app/home/home.controller.js
+++ client/app/home/home.controller.js
@@ -2,8 +2,9 @@
 
 angular.module('lolBetApp')
   .controller('HomeCtrl', ['$scope', 'Auth', function ($scope, Auth) {
-    $scope.message = 'Hello';
     $scope.user = Auth.getCurrentUser();
-    $scope.user.avatarURL = 'https://ddragon.leagueoflegends.com/cdn/4.13.1/img/profileicon/' + $scope.user.summoner.profileIconId + '.png';
-    console.log($scope.user);
+
+    if ($scope.user.summoner) {
+      $scope.user.avatarURL = 'https://ddragon.leagueoflegends.com/cdn/4.13.1/img/profileicon/' + $scope.user.summoner.profileIconId + '.png';
+    }
   }]);
