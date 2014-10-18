c6f56851770ad11b46fd20cff3c44da776c57d30
Marlon Kenny <marlonkenny@gmail.com>
2014-10-18 16:45:27 +0000

    fixed user icon issue by properly using a promise callback

--- client/app/home/home.controller.js
+++ client/app/home/home.controller.js
@@ -4,7 +4,7 @@ angular.module('lolBetApp')
   .controller('HomeCtrl', ['$scope', 'Auth', function ($scope, Auth) {
     $scope.user = Auth.getCurrentUser();
 
-    if ($scope.user.summoner) {
+    $scope.user.$promise.then(function() {
       $scope.user.avatarURL = 'https://ddragon.leagueoflegends.com/cdn/4.13.1/img/profileicon/' + $scope.user.summoner.profileIconId + '.png';
-    }
+    });
   }]);
