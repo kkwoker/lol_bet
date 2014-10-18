6cbb33bab49ad68266c05832cc522792f42249f2
Marlon Kenny <marlonkenny@gmail.com>
2014-10-18 00:07:19 +0000

    basic homepage up

--- client/app/home/home.controller.js
+++ client/app/home/home.controller.js
@@ -1,6 +1,9 @@
 'use strict';
 
 angular.module('lolBetApp')
-  .controller('HomeCtrl', function ($scope) {
+  .controller('HomeCtrl', ['$scope', 'Auth', function ($scope, Auth) {
     $scope.message = 'Hello';
-  });
+    $scope.user = Auth.getCurrentUser();
+    $scope.user.avatarURL = 'https://ddragon.leagueoflegends.com/cdn/4.13.1/img/profileicon/' + $scope.user.summoner.profileIconId + '.png';
+    console.log($scope.user);
+  }]);
