7c5eba55a8bad70cb1795ce15ca9477a2714e7ef
vagrant <vagrant@lighthouse-labs>
2014-10-20 04:16:17 +0000

    refactor search controller and view code

--- client/app/summoner_search/summoner_search.controller.js
+++ client/app/summoner_search/summoner_search.controller.js
@@ -6,23 +6,33 @@ angular.module('lolBetApp')
     $scope.message = 'Hello';
     $scope.user = [];
     $scope.user_object = [];
-    $scope.sortField = 'name';
    
-     $http.get('/api/users')
+    $http.get('/api/users')
       .success(function(data){
         for (var i = 0; i < data.length; i++ ) {
           if (data[i]['summoner']) {
-            $scope.user_object.push( { name: data[i]['summoner']['name'], profile_icon_id: data[i]['summoner']['profileIconId'], summoner_level: data[i]['summoner']['summonerLevel'] } );
+            $scope.user_object.push( { "name": data[i]['summoner']['name'], "profile_icon_id": data[i]['summoner']['profileIconId'], "summoner_level": data[i]['summoner']['summonerLevel'] } );
           }
-        }
-        console.log($scope.user_object);
+        };
+
+        $scope.typeOptions = [
+      { name: 'Name', value: 'name' }, 
+      { name: 'Profile Id', value: 'profile_icon_id'} ,
+      { name: 'Summoner Level', value: 'summoner_level' }
+    ];
+
+      console.log($scope.user_object);
         for (var y = 0; y < $scope.user.length; y++){
           console.log($scope.user[y]);
         }
         }).error(function(data) {
-        console.log(data);
-      });
-       }]);
+          console.log(data);
+        });
+}]);
+
+    
+
+
 
 
 
