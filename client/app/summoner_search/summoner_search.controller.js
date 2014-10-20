441e01595729c4ca19bf23f18491f2b149e9529f
vagrant <vagrant@lighthouse-labs>
2014-10-20 00:42:12 +0000

    list users in /summoners_search

--- client/app/summoner_search/summoner_search.controller.js
+++ client/app/summoner_search/summoner_search.controller.js
@@ -1,6 +1,28 @@
 'use strict';
 
 angular.module('lolBetApp')
-  .controller('SummonerSearchCtrl', function ($scope) {
+  .controller('SummonerSearchCtrl', ['$scope', '$http', '$location', 'Auth', 
+    function ($scope, $http, $location, Auth) {
     $scope.message = 'Hello';
-  });
+    $scope.user = [];
+   
+     $http.get('/api/users')
+      .success(function(data){
+
+        for (var i = 0; i < data.length; i++ ) {
+          if (data[i]['summoner']) {
+
+            $scope.user.push([ data[i]['summoner']['name'], data[i]['summoner']['profileIconId'], data[i]['summoner']['summonerLevel'], data[i]['email']]);
+          }
+        }
+        for (var y = 0; y < $scope.user.length; y++){
+          console.log($scope.user[y]);
+        }
+        
+    
+      }).error(function(data) {
+        console.log(data);
+      });
+       }]);
+
+
--- client/app/summoner_search/summoner_search.js
+++ client/app/summoner_search/summoner_search.js
@@ -9,4 +9,3 @@ angular.module('lolBetApp')
       });
   });
 
-
