4dc2d36c1cb88585bde0474524da7c16e549e63a
Marlon Kenny <marlonkenny@gmail.com>
2014-10-22 19:19:39 +0000

    added route

---
+++ client/app/btc_transaction/btc_transaction.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('lolBetApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/btc_transaction', {
+        templateUrl: 'app/btc_transaction/btc_transaction.html',
+        controller: 'BtcTransactionCtrl'
+      });
+  });
