1806007fc80b966a20c46f4887801b42757dedf5
Thomas McGuirk <thomas.mcguirk1@gmail.com>
2014-10-22 15:30:37 -0700

    Working on blockchain api

--- server/api/btc_transaction/btc_transaction.controller.js
+++ server/api/btc_transaction/btc_transaction.controller.js
@@ -1,6 +1,7 @@
 'use strict';
 var keys = require('../../config/local.env.js');
 var https = require('https');
+var request = require('request');
 
 var _ = require('lodash');
 var BtcTransaction = require('./btc_transaction.model');
@@ -27,30 +28,25 @@ exports.create = function(req, res) {
   console.log("connected test");
   var transaction = {};
   var user_id = req.body.user_id;
-  var callback_url = keys.DOMAIN + '/btc_transactions/confirm';
+  var callback_url = encodeURIComponent(keys.DOMAIN + '/btc_transactions/confirm');
   var url = 'https://blockchain.info/api/receive?method=create&address=' + keys.BITCOIN_ADDRESS +'&callback=' + callback_url;
 
-  https.get(url, function(res) {
-    if (res["destination"] !== keys.BITCOIN_ADDRESS) {
-      console.log('error: Incorrect receiving bitcoin addres' + res["destination"]);
+  request(url, function(error, response, body) {
+    if (error) { return res.json(400, error); };
+    if (body["destination"] === keys.BITCOIN_ADDRESS) {
+  
+      transaction.input_address = body["input_address"];
+      transaction.user_id = user_id;
+      transaction.pending = true;
+
+      BtcTransaction.create(transaction, function(err, btc_transaction) {
+        if(err) { return handleError(res, err); }
+        return res.json(201, btc_transaction);
+      });
+    } else {
+        console.log('error: Incorect receiving bitcoin address ' + body);
     }
-    transaction.input_address = res["input_address"];
-    transaction.user_id = user_id;
-    transaction.pending = true;
-    console.log("creating record:")
-
-    BtcTransaction.create(transaction, function(err, btc_transaction) {
-      if(err) { return handleError(res, err); }
-      console.log("creation successful" + btc_transaction);
-      return res.json(201, btc_transaction);
-    });
-
-
-  }).on('error', function(e) {
-    console.log("Error from Blockchains callback: " + e.message);
   });
-
-
 };
 
 //Get callback response from Blockchain.
